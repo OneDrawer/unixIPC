967c9cff5a9d00bfbfdbdef21c0ba889dc0d258c
u1095279131516 <1095279131516@tianchi.com>
2017-07-06 13:44:03 +0800

    first commit

---
+++ svmsg/slot.c
@@ -0,0 +1,14 @@
+#include	"unpipc.h"
+
+int
+main(int argc, char **argv)
+{
+	int		i, msqid;
+
+	for (i = 0; i < 1000000; i++) {
+		msqid = Msgget(IPC_PRIVATE, SVMSG_MODE | IPC_CREAT);
+		printf("msqid = %d\n", msqid);
+		Msgctl(msqid, IPC_RMID, NULL);
+	}
+	exit(0);
+}
